328c5f3fee0802479250283ea4fcdcca16b221bb
Krisofer Brink <kpgbrink@gmail.com>
2017-04-03 01:47:47 -0400

    stem grows

--- scripts/main.js
+++ scripts/main.js
@@ -14,38 +14,136 @@ camera.position.set( 2000, 750, 2000 );
 // Add camera controls
 const controls = new THREE.OrbitControls( camera );
 controls.enablePan = false;
-controls.minDistance = 1000.0;
+controls.minDistance = 10;;
 controls.maxDistance = 5000.0;
-controls.maxPolarAngle = Math.PI * 0.495;
+//controls.maxPolarAngle = Math.PI * 0.495;
 controls.target.set( 0, 0, 0 );
 
+// Add point light that circles
+const pointLight = new THREE.PointLight('#ffffff', .9, 10000, .9);
+pointLight.position.set(0, 250, 1000);
+const pointLightEuler = new THREE.Euler(0, .02, 0);
+scene.add(pointLight);
+const updatePointLightPosition = function() {
+    pointLight.position.applyEuler(pointLightEuler);
+}
 
-const geometry = new THREE.BoxGeometry( 200, 200, 200 );
-const material = new THREE.MeshPhongMaterial();
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+const stemGeometry = new THREE.BoxGeometry(1, 1, 1);
+const stemMaterial = new THREE.MeshPhongMaterial({color: '#0fff00'});
 
 
-const stemMaterial = new THREE.MeshPhongMaterial()
+
+class StemPart {
+    constructor(prevStemPart) {
+        this.object = new THREE.Mesh(stemGeometry, stemMaterial);
+        this.object.scale.set(10, 25, 10);
+        
+        const getRandRot = function () {return Math.random*.2 -.1};
+        if (prevStemPart) {
+            console.log('prevStemPart');
+            // copy prevStem rotation
+            //this.object.rotation.copy(prevStemPart);
+            // rotate randomly
+            this.object.rotation.x += .2;
+            this.object.rotation.y += .1;
+            this.object.rotation.z += .1;
+        }
+        
+        this.object.updateMatrix();
+    }
+    
+    // Get Middles for growing and animating the stem
+    getTopMiddle() {
+        return this.getMiddle(new THREE.Vector3(0, .5, 0));
+    }
+    getBottomMiddle() {
+        return this.getMiddle(new THREE.Vector3(0, -.5, 0));        
+    }
+    getMiddle(vecOut) {
+        vecOut.applyMatrix4(this.object.matrix);
+        return vecOut;
+    }
+    
+    // run after rotations
+    placeStem(prevStemPart) {
+        let connectTop = prevStemPart.getTopMiddle();
+        let connectBottom = this.getBottomMiddle();
+        this.object.position.copy(connectTop.sub(connectBottom));
+    }
+    
+    // Todo
+    animate() {
+        
+    }
+}
+
+class Stem {
+    constructor(stemMax=1000) {
+        // Object holder
+        this.stemObjects = [];
+        this.growing = true;
+        this.stemMax = stemMax;
+    }
+    
+    stemTip() {
+        return this.stemObjects[this.stemObjects.length-1];
+    }
+    
+    addObject() {
+        let stemPart = new StemPart(this.stemTip());
+        if (this.stemObjects.length > 0) {
+            stemPart.placeStem(this.stemTip());
+        } else {
+            stemPart.object.position.set(0, -500, 0);
+        }
+        scene.add(stemPart.object);
+        
+        console.log(stemPart);
+        console.log(this.stemObjects.length);
+        
+        this.stemObjects.push(stemPart);
+    }
+    
+    trimStem() {
+        scene.remove(this.stemObjects.pop().object);
+    }
+    
+    // Checks growing. Runs on update
+    checkGrowing() {
+        if (this.growing) {
+            return this.stemObjects.length < this.stemMax;
+        } else {
+            return !(this.stemObjects.length > 0);
+        }
+    }
+    
+    update() {
+        this.growing = this.checkGrowing();
+        if (this.growing) {
+            this.addObject();
+        } else {
+            this.trimStem();
+        }
+    }
+}
+
 
 class FlowerScene {
     constructor() {
-        // Object holder
-        this.stemObject = [];
+        this.stem = new Stem();
         this.render();
     }
-    
-    addStemObject() {
-        let newObject = new THREE.BoxGeometry( 100, 100, 100);
-        
-        this.stemObject.push();
-    }
-    
 
     render() {
       requestAnimationFrame( () => this.render() );
-      cube.rotation.x += 0.0;
-      cube.rotation.y += 0.0;
+      // Update the stem
+      this.stem.update();
+       
+        
+        
+      updatePointLightPosition();
+        
       renderer.render( scene, camera );
     }
 }
